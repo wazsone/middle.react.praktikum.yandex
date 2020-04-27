0541f63258712632346ff1f426991cbee3916b8f
Ruslan Bayandinov <rbayandi@cisco.com>
2020-04-27 23:15:14 +0700

    Add auto code check
    remove unused props var

--- src/components/Chat/Preview/List.tsx
+++ src/components/Chat/Preview/List.tsx
@@ -3,11 +3,10 @@ import { IChatPreviewItem, Item } from "./Item/Item";
 import "./List.css";
 
 interface IProps {
-    className?: string;
     data: IChatPreviewItem[];
 }
 
-export const List: React.FC<IProps> = ({ className, data }) => {
+export const List: React.FC<IProps> = ({ data }) => {
     const renderItems = () => {
         return data.map((itemProps, id) => <Item key={id} {...itemProps} />);
     };
