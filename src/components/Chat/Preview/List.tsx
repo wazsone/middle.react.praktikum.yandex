ea79bdf56a5bbe0016667044240193c3bae475a2
Ruslan Bayandinov <rbayandi@cisco.com>
2020-04-28 09:14:30 +0700

    Sort chats by date

--- src/components/Chat/Preview/List.tsx
+++ src/components/Chat/Preview/List.tsx
@@ -8,7 +8,9 @@ interface IProps {
 
 export const List: React.FC<IProps> = ({ data }) => {
     const renderItems = () => {
-        return data.map((itemProps, id) => <Item key={id} {...itemProps} />);
+        return data
+            .sort((a, b) => b.date.getTime() - a.date.getTime())
+            .map((itemProps, id) => <Item key={id} {...itemProps} />);
     };
 
     return <div className="flex chat-preview-list">{renderItems()}</div>;
