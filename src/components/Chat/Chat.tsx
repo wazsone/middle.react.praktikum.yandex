75a1382ebecc9ad89b2c8fdea24683455f07ad88
Ruslan Bayandinov <rbayandi@cisco.com>
2020-05-01 00:21:01 +0700

    Add chat screen
    move state from preview to chat component

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { generateChatData, IChatGroup, IChatPreviewItem } from "../../TestData";
 import { Preview } from "./Preview/Preview";
+import { Message } from "./Screen/Message/Message";
 import "./Chat.css";
 
 interface IState {
     chats: IChatGroup[];
+    activeChat: number;
 }
 
 export class Chat extends React.Component<{}, IState> {
     readonly state: IState = {
         chats: [],
+        activeChat: -1,
+    };
+
+    private selectChat = (chatId: number) => {
+        this.setState({ activeChat: chatId });
     };
 
     componentDidMount() {
@@ -28,10 +35,24 @@ export class Chat extends React.Component<{}, IState> {
     };
 
     render() {
+        const { chats, activeChat } = this.state;
+        const messages =
+            activeChat >= 0 && chats.length > 0
+                ? chats[activeChat].messages
+                : undefined;
         return (
             <div className="flex chat">
-                <Preview data={this.preparePreviewData()} />
-                <div className="flex chat-screen">{"Some random text"}</div>
+                <Preview
+                    data={this.preparePreviewData()}
+                    activeItem={activeChat}
+                    selectItem={this.selectChat}
+                />
+                <div className="flex chat-screen">
+                    {messages &&
+                        messages.map((message, id) => (
+                            <Message key={id} {...message} />
+                        ))}
+                </div>
             </div>
         );
     }
--- src/components/Chat/Preview/Preview.tsx
+++ src/components/Chat/Preview/Preview.tsx
@@ -5,29 +5,18 @@ import { IChatPreviewItem } from "../../../TestData";
 
 interface IProps {
     data: IChatPreviewItem[];
+    activeItem: number;
+    selectItem: (itemId: number) => void;
 }
 
-interface IState {
-    activeItemId: number;
-}
-
-export class Preview extends React.Component<IProps, IState> {
-    readonly state: IState = {
-        activeItemId: -1,
-    };
-
-    private selectItem = (itemId: number) => {
-        this.setState({ activeItemId: itemId });
-    };
-
+export class Preview extends React.Component<IProps> {
     private renderItems = () => {
-        const { data } = this.props;
-        const { activeItemId } = this.state;
+        const { data, selectItem, activeItem } = this.props;
         return data.map((itemData, id) => (
             <Item
                 key={id}
-                isActive={id === activeItemId}
-                selectItem={() => this.selectItem(id)}
+                isActive={id === activeItem}
+                selectItem={() => selectItem(id)}
                 {...itemData}
             />
         ));
