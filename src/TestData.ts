2e3339b179cf348452d1751e8ea4c10b5ceb76c8
Ruslan Bayandinov <rbayandi@cisco.com>
2020-04-30 21:41:07 +0700

    Change test data

--- src/TestData.ts
+++ src/TestData.ts
@@ -1,5 +1,3 @@
-import logo from "./img/logo.png";
-
 const randomDate = (start: Date, end: Date) => {
     return new Date(
         start.getTime() + Math.random() * (end.getTime() - start.getTime())
@@ -14,195 +12,77 @@ export interface IChatPreviewItem {
     message: string;
 }
 
-// test data
-export const testData: IChatPreviewItem[] = [
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
+export interface IRandomUserName {
+    title: string;
+    first: string;
+    last: string;
+}
+
+export interface IRandomUserIcon {
+    large: string;
+    medium: string;
+    thumbnail: string;
+}
+
+export interface IRandomUser {
+    name: IRandomUserName;
+    picture: IRandomUserIcon;
+}
+
+const getRandomUsers = () => {
+    const usersAmount = Math.ceil(Math.random() * 10);
+    const url = `https://randomuser.me/api/?results=${usersAmount}&inc=name,picture&noinfo`;
+    return fetch(url)
+        .then((res) => res.json())
+        .then((json) => json.results);
+};
+
+export interface IMessage {
+    icon: string;
+    author: string;
+    message: string;
+    date: Date;
+}
+
+export interface IChatGroup {
+    name: string;
+    messages: IMessage[];
+}
+
+const generateMessage = (user: IRandomUser): IMessage => {
+    return {
+        icon: user.picture.thumbnail,
+        author: `${user.name.last} ${user.name.first}`,
         message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
         date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
+    };
+};
+
+const generateGroupChat = (users: IRandomUser[]): IChatGroup => {
+    const messages: IMessage[] = [];
+    const messagesAmount = Math.ceil(5 + Math.random() * 5);
+    let rndUsers = Object.assign([], users);
+    for (let count = 0; count < messagesAmount; count++) {
+        const usrId = Math.floor(Math.random() * rndUsers.length);
+        messages.push(generateMessage(rndUsers[usrId]));
+        if (rndUsers.length === 1) {
+            rndUsers = Object.assign([], users);
+        } else {
+            rndUsers.splice(usrId, 1);
+        }
+    }
+    return {
         name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-    {
-        icon: logo,
-        name: "Group Chat",
-        date: randomDate(new Date(2019, 0, 1), new Date()),
-        author: "Ruslan",
-        message: "Hello World! I am going to use all this line! Why not?",
-    },
-];
+        messages: messages.sort((a, b) => a.date.getTime() - b.date.getTime()),
+    };
+};
+
+export const generateChatData = async (): Promise<IChatGroup[]> => {
+    const users = await getRandomUsers();
+    const chatsAmount = Math.ceil(10 + Math.random() * 10);
+    const chats: IChatGroup[] = [];
+    for (let count = 0; count < chatsAmount; count++) {
+        chats.push(generateGroupChat(users));
+    }
+    return chats;
+};
--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { testData } from "../../TestData";
+import { generateChatData, IChatGroup, IChatPreviewItem } from "../../TestData";
 import { Preview } from "./Preview/Preview";
 import "./Chat.css";
 
-export const Chat: React.FC = () => {
-    return (
-        <div className="flex chat">
-            <Preview data={testData} />
-            <div className="flex chat-screen">{"Some random text"}</div>
-        </div>
-    );
-};
+interface IState {
+    chats: IChatGroup[];
+}
+
+export class Chat extends React.Component<{}, IState> {
+    readonly state: IState = {
+        chats: [],
+    };
+
+    componentDidMount() {
+        generateChatData().then((chats) => this.setState({ chats: chats }));
+    }
+
+    private preparePreviewData = (): IChatPreviewItem[] => {
+        const { chats } = this.state;
+        const data = chats.map((chat) => {
+            return {
+                name: chat.name,
+                ...chat.messages[chat.messages.length - 1],
+            };
+        });
+        return data.sort((a, b) => b.date.getTime() - a.date.getTime());
+    };
+
+    render() {
+        return (
+            <div className="flex chat">
+                <Preview data={this.preparePreviewData()} />
+                <div className="flex chat-screen">{"Some random text"}</div>
+            </div>
+        );
+    }
+}
